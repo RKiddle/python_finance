4332b0ba82e1726744ffc4c0c4ca3f62e2c7edd7
RKiddle <RKiddle@users.noreply.github.com>
2019-07-23 20:03:44 +0700

    Create Select_the_top_5_listed_consumer_companies.py

---
+++ Importing_&_Managing_Financial_Data/Importing_financial_data_from_the_web/Select_the_top_5_listed_consumer_companies.py
@@ -0,0 +1,8 @@
+# Select companies in Consumer Services
+consumer_services = listings[listings.Sector == 'Consumer Services']
+
+# Sort consumer_services by market cap
+consumer_services2 = consumer_services.sort_values('Market Capitalization', ascending=False)
+
+# Display first 5 rows of designated columns
+print(consumer_services2[['Company Name', 'Exchange', 'Market Capitalization']].head())

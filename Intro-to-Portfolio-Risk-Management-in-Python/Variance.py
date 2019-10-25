72b872e9ddb13c8984df7b8f13966cb28b9cf065
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:29:16 +0700

    Create Variance.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Variance.py
@@ -0,0 +1,7 @@
+# Calculate the standard deviation of daily return of the stock
+sigma_daily = np.std(StockPrices['Returns'])
+print(sigma_daily)
+
+# Calculate the daily variance
+variance_daily = sigma_daily**2
+print(variance_daily)

4627710719e759fb0b9e7d8de671edc71625be09
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:31:56 +0700

    Create Annualizing-variance.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Annualizing-variance.py
@@ -0,0 +1,7 @@
+# Annualize the standard deviation
+sigma_annualized = sigma_daily*np.sqrt(252)
+print(sigma_annualized)
+
+# Calculate the annualized variance
+variance_annualized = sigma_annualized**2
+print(variance_annualized)

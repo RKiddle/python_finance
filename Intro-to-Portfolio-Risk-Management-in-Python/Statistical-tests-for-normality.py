3d6564fc1eb93b6114bb2a2c9cdbe8510a010411
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:44:43 +0700

    Create Statistical-tests-for-normality.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Statistical-tests-for-normality.py
@@ -0,0 +1,10 @@
+# Import shapiro from scipy.stats
+from scipy.stats import shapiro
+
+# Run the Shapiro-Wilk test on the stock returns
+shapiro_results = shapiro(clean_returns)
+print("Shapiro results:", shapiro_results)
+
+# Extract the p-value from the shapiro_results
+p_value = shapiro_results[1]
+print("P-value: ", p_value)

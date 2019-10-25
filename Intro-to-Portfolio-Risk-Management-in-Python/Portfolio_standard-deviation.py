265cebc11ed05431cb62df0e3df44a89d1aecdfc
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 15:49:44 +0700

    Create Portfolio_standard-deviation.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Portfolio_standard-deviation.py
@@ -0,0 +1,6 @@
+# Import numpy as np
+import numpy as np
+
+# Calculate the portfolio standard deviation
+portfolio_volatility = np.sqrt(np.dot(portfolio_weights.T, np.dot(cov_mat_annual, portfolio_weights)))
+print(portfolio_volatility)

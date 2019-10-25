ec1af0681380bfaa902755334902e00ea1e50d0a
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:38:40 +0700

    Create Skewness.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Skewness.py
@@ -0,0 +1,9 @@
+# Import skew from scipy.stats
+from scipy.stats import skew
+
+# Drop the missing values
+clean_returns = StockPrices['Returns'].dropna()
+
+# Calculate the third moment (skewness) of the returns distribution
+returns_skewness = skew(clean_returns)
+print(returns_skewness)

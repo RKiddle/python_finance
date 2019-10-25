f5ba7d8a5d9a908efd08b0c0e0cd235a2f10c3c5
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:40:43 +0700

    Create Kurtosis.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Kurtosis.py
@@ -0,0 +1,10 @@
+# Import kurtosis from scipy.stats
+from scipy.stats import kurtosis
+
+# Calculate the excess kurtosis of the returns distribution
+excess_kurtosis = kurtosis(clean_returns)
+print(excess_kurtosis)
+
+# Derive the true fourth moment of the returns distribution
+fourth_moment = excess_kurtosis + 3
+print(fourth_moment)

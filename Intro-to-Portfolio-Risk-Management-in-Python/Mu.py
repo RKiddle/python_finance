da42fa5dc8bad784eeca487b234377c41a9a2a5c
RKiddle <RKiddle@users.noreply.github.com>
2019-10-25 14:17:29 +0700

    Create Mu.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Mu.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# Calculate the average daily return of the stock
+mean_return_daily = np.mean(StockPrices['Returns'])
+print(mean_return_daily)
+
+# Calculate the implied annualized average return
+mean_return_annualized = ((1+mean_return_daily)**252)-1
+print(mean_return_annualized)

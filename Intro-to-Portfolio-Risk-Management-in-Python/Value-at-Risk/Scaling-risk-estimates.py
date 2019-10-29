adcb6c59cba49afed75f44f07753326e365e1088
RKiddle <RKiddle@users.noreply.github.com>
2019-10-29 16:16:25 +0700

    Update Scaling-risk-estimates.py

--- Intro-to-Portfolio-Risk-Management-in-Python/Value-at-Risk/Scaling-risk-estimates.py
+++ Intro-to-Portfolio-Risk-Management-in-Python/Value-at-Risk/Scaling-risk-estimates.py
@@ -3,3 +3,23 @@ StockReturns_perc and var_95 from the previous exercise is available in your wor
 We've also defined a function plot_var_scale() that plots the VaR for 1 to 100 days from now.
 """"
 
+def plot_var_scale():
+    # Plot the forecased vs time
+    plt.plot(forecasted_values[:,0], -1*forecasted_values[:,1])
+    plt.xlabel('Time Horizon T+i')
+    plt.ylabel('Forecasted VaR 95 (%)')
+    plt.title('VaR 95 Scaled by Time', fontsize=18, fontweight='bold')
+    plt.show()
+    
+# Aggregate forecasted VaR
+forecasted_values = np.empty([100, 2])
+
+# Loop through each forecast period
+for i in range(0, 100):
+    # Save the time horizon i
+    forecasted_values[i, 0] = i
+    # Save the forecasted VaR 95
+    forecasted_values[i, 1] = var_95*np.sqrt(i+1)
+    
+# Plot the results
+plot_var_scale()

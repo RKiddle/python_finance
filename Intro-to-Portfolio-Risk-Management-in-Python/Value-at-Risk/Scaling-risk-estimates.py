9d3006b89483fe0b0a5082f1539f1f2f7dc31af7
RKiddle <RKiddle@users.noreply.github.com>
2019-10-29 16:10:18 +0700

    Create Scaling-risk-estimates.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Value-at-Risk/Scaling-risk-estimates.py
@@ -0,0 +1,5 @@
+""""
+StockReturns_perc and var_95 from the previous exercise is available in your workspace. Use this data to estimate the VaR for the USO oil ETF for 1 to 100 days from now. 
+We've also defined a function plot_var_scale() that plots the VaR for 1 to 100 days from now.
+""""
+

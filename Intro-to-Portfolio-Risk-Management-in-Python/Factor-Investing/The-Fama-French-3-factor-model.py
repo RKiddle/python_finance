5e277047b39c8f8c4c5d98e4307084a547744a74
RKiddle <RKiddle@users.noreply.github.com>
2019-10-29 14:26:10 +0700

    Create The-Fama-French-3-factor-model.py

---
+++ Intro-to-Portfolio-Risk-Management-in-Python/Factor-Investing/The-Fama-French-3-factor-model.py
@@ -0,0 +1,12 @@
+# Import statsmodels.formula.api
+import statsmodels.formula.api as smf 
+
+# Define the regression formula
+FamaFrench_model = smf.ols(formula='Portfolio_Excess~Market_Excess~SMB~HML', data=FamaFrenchData)
+
+# Fit the regression
+FamaFrench_fit = FamaFrench_model.fit()
+
+# Extract the adjusted r-squared
+regression_adj_rsq = FamaFrench_fit.rsquared_adj
+print(regression_adj_rsq)

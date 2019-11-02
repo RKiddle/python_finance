a0eb23a332216af5c25b7006fb934e2acb615686
RKiddle <RKiddle@users.noreply.github.com>
2019-11-02 10:33:28 +0700

    Create Calculate-mean-returns.py

---
+++ Introduction-to-Portfolio-Analysis/Calculate-mean-returns.py
@@ -0,0 +1,26 @@
+""""
+
+Calculate the percentage returns of the stocks in the DataFrame data by comparing today's price with yesterday's price.
+Calculate the mean returns of each stock in the new returns DataFrame.
+Assign the weights of the stocks to the weights array. The weights are 0.5, 0.2, 0.2 and 0.1.
+Multiply the percentage returns with the weights, and take the total sum, 
+to calculate the total portfolio performance and print the results.
+
+""""
+
+
+
+# Calculate percentage returns
+returns = data.pct_change()
+
+# Calculate individual mean returns 
+meanDailyReturns = returns.mean()
+
+# Define weights for the portfolio
+weights = np.array([ ____, ____, ____])
+
+# Calculate expected portfolio performance
+portReturn = np.____(____*____)
+
+# Print the portfolio return
+print(____)

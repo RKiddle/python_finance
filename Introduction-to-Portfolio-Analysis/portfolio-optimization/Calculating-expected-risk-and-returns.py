ef05202e9435645150254341297780c370391015
RKiddle <RKiddle@users.noreply.github.com>
2019-11-07 21:07:46 +0700

    Create Calculating-expected-risk-and-returns.py

---
+++ Introduction-to-Portfolio-Analysis/portfolio-optimization/Calculating-expected-risk-and-returns.py
@@ -0,0 +1,14 @@
+# Import the packages 
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.efficient_frontier import EfficientFrontier
+
+# Calculate expected returns mu 
+mu = expected_returns.mean_historical_return(stock_prices)
+
+# Calculate the covariance matrix S
+Sigma = risk_models.sample_cov(stock_prices)
+
+# Obtain the efficient frontier
+ef = EfficientFrontier(mu, Sigma)
+print (mu, Sigma)

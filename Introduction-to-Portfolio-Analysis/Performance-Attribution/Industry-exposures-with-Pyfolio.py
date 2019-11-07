e45960fb469132d30e2ef7b649d556bfc964e642
RKiddle <RKiddle@users.noreply.github.com>
2019-11-07 20:54:20 +0700

    Create Industry-exposures-with-Pyfolio.py

---
+++ Introduction-to-Portfolio-Analysis/Performance-Attribution/Industry-exposures-with-Pyfolio.py
@@ -0,0 +1,12 @@
+# Define sector mappings
+sect_map = {'COST': 'Consumer Goods',
+            'INTC': 'Technology', 
+            'CERN': 'Healthcare', 
+            'GPS': 'Technology',
+            'MMM': 'Construction', 
+            'DELL': 'Technology', 
+            'AMD': 'Technology'}
+
+# Create sector exposure tear sheet
+pf.create_position_tear_sheet(returns, positions, sector_mappings=sect_map)
+display_tear_sheet()
